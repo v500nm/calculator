0270f2e06c77428aae509dfd05e282e4adce45eb
Adnan Mangaonkar <adnans0307@gmail.com>
2022-07-14 20:45:03 +0530

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { HostListener, Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+
+  Answer:any="";
+  num(value:any){
+    this.Answer=this.Answer+value.target.value;
+  }
+  equal(){
+    this.Answer=eval(this.Answer)
+  }
+  refresh(){
+    this.Answer=''
+  }
+  calcValue(solve:any){
+    this.Answer=eval(solve)
+  }
+  @HostListener('document:keypress',['$event'])
+  handleKeyboardEvent(event:KeyboardEvent){
+    console.log(event.key);
+    if(event.key=='Enter'){
+      this.Answer=eval(this.Answer)
+    }else{
+this.Answer=this.Answer+event.key
+    }
+  }
 }
